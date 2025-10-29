9bbd136e46fac8d84fa10427da6aa589fa0276df
hamzacs50com <143846712+hamzacs50com@users.noreply.github.com>
2025-10-29 09:16:47 +0300

    Update App.java

--- StudentManagementSystem/src/gui/App.java
+++ StudentManagementSystem/src/gui/App.java
@@ -1,13 +1,13 @@
 package gui;
 
-import backend.StudentManager; // Import your backend
+import backend.StudentManager; // Import our backend
 import javax.swing.SwingUtilities;
 
 public class App {
     public static void main(String[] args) {
         // Use SwingUtilities to ensure the GUI starts safely
         SwingUtilities.invokeLater(() -> {
-            // 1. Create the one and only instance of the "engine"
+            // 1. Create the one and only instance of the "backend"
             StudentManager manager = new StudentManager();
 
             // 2. Create the main window and pass the engine to it
